5d393445b33dd067091b59a0dee88f3f86eafce3
lcf <kevinfinley@163.com>
2023-11-04 15:39:04 +0800

    transforms bug fix

--- train.py
+++ train.py
@@ -32,7 +32,7 @@ if args.use_augmentation:
 
 train_transforms.extend([
     transforms.ToTensor(),
-    transforms.Normalize(mean=args.mean, std=args.std),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 transform_train = transforms.Compose(train_transforms)
